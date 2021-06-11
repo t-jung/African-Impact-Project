eccc62b10fe12a66f60c4810c417d2f48a9ad397
Eugene Koo <eugenekoo777@gmail.com>
2021-06-10 23:20:47 -0400

    Fixed syntax error in front page app

--- c01/src/App.js
+++ c01/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import LoginForm from './LoginComponents/LoginForm'
-import ProfileForm from './ProfileComponents/ProfileForm'
+import ProfileEditForm from './ProfileComponents/ProfileEditForm'
 import RegisterForm from './RegisterComponents/RegisterForm'
+import AdminPage from './AdminComponents/AdminPage';
+import ProfileFrame from './ProfileComponents/ProfileFrame';
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -11,7 +13,9 @@ function App() {
     <div className="App">
       <LoginForm /><br/><br/><br/><br/>
       <RegisterForm /><br/><br/><br/><br/>
-      <ProfileForm /> <br/><br/><br/><br/>
+      <ProfileFrame /> <br/><br/><br/><br/>
+      <ProfileEditForm /> <br/><br/><br/><br/>
+      <AdminPage /> <br/><br/><br/><br/>
     </div>
   );
 }
