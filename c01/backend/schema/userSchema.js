ad41f550ee462c387c612a000926cf42eb6a02f7
Jason Yu <jason.yu2000@hotmail.com>
2021-07-01 17:26:52 -0400

    Added userSchema from old CSCCPROJ-47 branch

---
+++ c01/backend/schema/userSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: { type: String, required: true, 
+    trim: true},
+    lastName: { type:String, required: true, trim:true},
+    gender: {
+        type: String
+    },
+    phoneNumber: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    email: {
+        type: String, required: true
+    },
+    password: {
+        type: String, required: true
+    },
+    posts: {
+        tags: [{
+            type: String
+        }]
+    },
+    followers: {
+        tags: [{
+            type: String
+        }]
+    }
+
+});
+
+const User = mongoose.model("User",userSchema);
+
+module.exports = User;
