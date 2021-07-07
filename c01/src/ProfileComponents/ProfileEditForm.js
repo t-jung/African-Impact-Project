f25ce3d9ae41487bf73b3445e2ff08e07d70eca6
Cheryl <cheryl.pl.chen@hotmail.com>
2021-07-08 07:37:00 +0800

    Added profile edit

--- c01/src/ProfileComponents/ProfileEditForm.js
+++ c01/src/ProfileComponents/ProfileEditForm.js
@@ -1,10 +1,58 @@
 import './ProfileFrame.css'
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles.js';
 import { Typography } from '@material-ui/core';
 
 const EditForm = (props) => {
     const user = props.user;
+
+    const [firstName, setFirstName] = useState(user.firstName);
+    const [middleName, setMiddleName] = useState(user.middleName);
+    const [lastName, setLastName] = useState(user.lastName);
+    const [email, setEmail] = useState(user.serEmail);
+    const [confirmEmail, setConfirmEmail] = useState(user.serEmail);
+    const [phone, setPhone] = useState(user.userPhone);
+    const [description, setDescription] = useState(user.description);
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        
+        if(!firstName) {
+            alert('A first name is required.')
+            setFirstName('')
+            return
+        }
+
+        if(!lastName) {
+            alert('A last name is required.')
+            setLastName('')
+            return
+        }
+        
+        
+        if(!email) {
+            alert('An email is required.')
+            setEmail('')
+            return
+        }
+
+        if(!phone) {
+            alert('A company phone number is required for registration.')
+            setPhone('')
+            return
+        }
+
+        if(email !== confirmEmail) {
+            alert('Confirmation email and email don\'t match')
+            setConfirmEmail('');
+            return
+        }
+
+        console.log('Successful.')        
+        console.log(`Name: ${firstName}`)
+        console.log(`Email: ${email}`)
+    }
+
     return(
     <div class="formContainer card">
         <Typography style={{
@@ -21,7 +69,8 @@ const EditForm = (props) => {
                     id="fNameID"
                     class="form-control"
                     placeholder="Edit first name"
-                    defaultValue={user.firstName}/>
+                    defaultValue={user.firstName}
+                    onChange={(e) => setFirstName(e.target.value)}/>
             </div>
             <div class="d-flex inputContainer">
                 <label class="title  align-self-center" for="fName">Middle name: </label>
@@ -29,7 +78,8 @@ const EditForm = (props) => {
                     id="mNameID"
                     class="form-control"
                     placeholder="Edit middle name"
-                    defaultValue={user.middleName}/>
+                    defaultValue={user.middleName}
+                    onChange={(e) => setMiddleName(e.target.value)}/>
             </div>
             <div class="d-flex inputContainer">
                 <label class="title  align-self-center" for="lName">Last name: </label>
@@ -37,7 +87,8 @@ const EditForm = (props) => {
                     id="lNameID"
                     class="form-control"
                     placeholder="Edit last name"
-                    defaultValue={user.lastName}/>
+                    defaultValue={user.lastName}
+                    onChange={(e) => setLastName(e.target.value)}/>
             </div>
             <div class="d-flex inputContainer">
                 <label class="title  align-self-center" for="email">Email: </label>
@@ -45,14 +96,16 @@ const EditForm = (props) => {
                     id="emailID"
                     class="form-control"
                     placeholder="Edit email"
-                    defaultValue={user.serEmail}/>
+                    defaultValue={user.serEmail}
+                    onChange={(e) => setEmail(e.target.value)}/>
             </div>
             <div class="d-flex inputContainer">
                 <label class="title  align-self-center" for="email">Confirm email: </label>
                 <input type="text"
                     id="emailID"
                     class="form-control"
-                    placeholder="Confirm email"/>
+                    placeholder="Confirm email"
+                    onChange={(e) => setConfirmEmail(e.target.value)}/>
             </div>
             <div class="d-flex inputContainer">
                 <label class="title  align-self-center" for="phone">Phone number: </label>
@@ -60,7 +113,8 @@ const EditForm = (props) => {
                     id="phoneID"
                     class="form-control"
                     placeholder="Phone number"
-                    defaultValue={user.userPhone}/>
+                    defaultValue={user.userPhone}
+                    onChange={(e) => setPhone(e.target.value)}/>
             </div>
             <div class="d-flex inputContainer">
                 <label class="title  align-self-center" for="email">Description: </label>
@@ -68,10 +122,11 @@ const EditForm = (props) => {
                     rows={4}
                     class="form-control multiText"
                     name="body"
-                    defaultValue={user.description}/>
+                    defaultValue={user.description}
+                    onChange={(e) => setDescription(e.target.value)}/>
             </div>
             <div class="d-flex justify-content-center">
-                <a href="/profile" button class="btn" type="submit" value="Save!">Edit</a>
+                <a href="/profile" button class="btn" type="submit" value="Save!" onClick={onSubmit}>Edit</a>
             </div>        
         </form>
     </div>
