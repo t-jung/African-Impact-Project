82514f0ca2ee2bf3f5e6c1b703d93f4a79103165
Cheryl <cheryl.pl.chen@hotmail.com>
2021-07-08 06:34:26 +0800

    Cleaned up SingleFeed.js

--- c01/src/FeedComponents/SingleFeed.js
+++ c01/src/FeedComponents/SingleFeed.js
@@ -4,24 +4,6 @@ import UserPost from './UserPostComponents/UserPost.js'
 export default function SingleFeed (props) {
     let feedList = props.feedList
 
-    const GetFeed = (props) => {
-        let feed = props.feed;
-        return (
-            <div class="card">
-                <div class="card-body">
-                    <div class="d-flex">
-                        <img src={feed.img} className="barProfilePic" />
-                        <div>
-                            <h5>{feed.userName}</h5>
-                            {feed.content}
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
     return(
         <div>
             {feedList.map(item => (
