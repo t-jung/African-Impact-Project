13d8772e27150f228821357c99d9325572575bb6
UGM <yeonohjung1@gmail.com>
2021-07-15 11:08:54 -0400

    elearn

---
+++ c01/src/ELearningComponents/VideoFeed.js
@@ -0,0 +1,128 @@
+import './VideoFeed.css'
+import Video from './Video'
+
+const VideoFeed = () => {
+    return (
+        <div className="videoFeed">
+            <h2>Videos</h2>
+            <div className="videoFeedVideos">
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                <Video 
+                    title="Better Jungler Wins"
+                    views="1.8M Views"
+                    timestamp="25 Apr 2019"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLT4JR4XDBa2K6gSGUCo28kqWkTWMEchrcd1ypj7=s88-c-k-c0x00ffffff-no-rj"
+                    channel="Shieda"
+                    image="https://i.ytimg.com/vi/wXMD6wmcwvE/maxresdefault.jpg"
+                />
+                
+
+            </div>
+        </div>
+    )
+}
+
+export default VideoFeed
