19bc0c3811aec196a5adf7e51adb69bbd90ebd22
Eugene Koo <eugenekoo777@gmail.com>
2021-07-18 21:59:06 -0400

    CSCCPROJ-62 css fixes

--- c01/src/ChatRoomComponents/MessagesComponent/MessagesComponent.js
+++ c01/src/ChatRoomComponents/MessagesComponent/MessagesComponent.js
@@ -3,12 +3,12 @@ import {format} from "timeago.js"
 
 export default function MessagesComponent({message, own}) {
     return (
-        <div className={own ? "message own" : "message"}>
-            <div className="messageTop">
-                <img className="messageImg" src="https://cdn.discordapp.com/attachments/829661320923447326/860355801931579422/unknown.png" alt=""/>
-                <p className="messageText">{message.text}</p>
+        <div className={own ? "messages own" : "messages"}>
+            <div className="messagesTop">
+                <img className="messagesImg" src="https://cdn.discordapp.com/attachments/829661320923447326/860355801931579422/unknown.png" alt=""/>
+                <p className="messagesText">{message.text}</p>
             </div>
-            <div className="messageBottom">{format(message.createdAt)}</div>
+            <div className="messagesBottom">{format(message.createdAt)}</div>
         </div>
     )
 }
