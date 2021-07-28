79981a0326ced58f66d37e4658423bb9d1264b89
Cheryl <cheryl.pl.chen@hotmail.com>
2021-07-28 21:16:19 +0800

    Styled view uploaded assignments

--- c01/src/CourseUploadComponents/ViewUploadedAssignments/ViewUploadedAssignments.js
+++ c01/src/CourseUploadComponents/ViewUploadedAssignments/ViewUploadedAssignments.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styles from '../../styles'
+import './ViewUploadedAssignments.css'
 
 import { Avatar, ListItemAvatar, List, ListItem, ListItemText, IconButton,
      Grid, Typography} from '@material-ui/core';
@@ -14,20 +15,43 @@ import { ThemeProvider, makeStyles } from '@material-ui/styles';
 
 const videoId = sessionStorage.getItem('videoId')
 
+let classNameHolder = ['greenAvatar', 'yellowAvatar', 'pinkAvatar', 'blueAvatar']
+
 const useStyles = makeStyles((theme) => ({
-    avatarStyle: {
-        marginRight: 10,
-    }, 
-}));
+    avatarStyle:{
+        fontSize: 18,
+        color: styles.palette.primary.main,
+        fontWeight: 100,
+        border: '4px solid orange',
+        marginRight: 8,
+    },
+    greenAvatar:{
+        backgroundColor: styles.palette.green.main,
+    },
+    yellowAvatar:{
+        backgroundColor: styles.palette.yellowLemon.main,
+    },
+    pinkAvatar:{
+        backgroundColor: styles.palette.pink.main,
+    },
+    blueAvatar:{
+        backgroundColor: styles.palette.blue.main,
+    },
+  }));
 
 const UserList = (props) => {
     const list = props.list
     const classes = useStyles()
+
+    const onDownload = (props) => {
+        return download()
+    }
+
     return(
         <List>
             {list.map(item => (
                 <ListItem key={item.uploader}>
-                    <Avatar className={classes.avatarStyle}>{item.uploader[0].toUpperCase()}</Avatar>
+                    <Avatar className={[classes.avatarStyle, classes[classNameHolder[Math.floor(Math.random() * classNameHolder.length)]]]}>{item.uploader[0].toUpperCase()}</Avatar>
                     <ListItemText primary={item.filename} secondary={item.uploader}/>
                     <IconButton>
                         <GetAppIcon/>
@@ -41,7 +65,7 @@ const UserList = (props) => {
 let videoTitle = sessionStorage.getItem('videoTitle')
 
 const ViewAssignments = () => {
-    let header = 'Uploaded assignments for ' + videoTitle
+    let header = 'View uploaded assignments for ' + videoTitle
     return(
         <div class="admin-topbar">
             <ThemeProvider theme={styles}>
@@ -49,7 +73,7 @@ const ViewAssignments = () => {
                     <Grid item direction="row">
                         <div class="admin-sidebyside">
                             <IconButton>
-                            <a href="/adminpage" className="backArrowButton"><ArrowBackIcon/></a>
+                                <a href="/adminpage"><ArrowBackIcon/></a>
                             </IconButton>
                             <Typography style={{
                                 color: styles.palette.primary.main,
@@ -62,7 +86,48 @@ const ViewAssignments = () => {
                     </Grid>
                 </Grid>
             </ThemeProvider>
-            </div>
+        </div>
+    )
+}
+
+const Upload = (props) => {
+
+    const[file, setFile] = React.useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const data = new FormData();
+        data.append('file', file)
+
+        console.log(data)
+
+    }
+
+    const selectFile = (e) => {
+        setFile(e.target.files[0])
+    }
+
+    const onDownload = () => {
+        return download(file, "test", 'multipart/form-data')
+    }
+
+    return(
+        <div class="elearning-upload-container">
+            <form onSubmit={onSubmit}>
+                <input
+                    id="upload-btn"
+                    class="elearning-button"
+                    type="file"
+                    onChange={selectFile}
+                /> 
+                <input
+                    type='submit'
+                    class="elearning-button"
+                />
+                <button onClick={onDownload}>click</button>
+            </form>
+        </div>
     )
 }
 
@@ -87,8 +152,10 @@ class ViewUploadedAssignments extends Component {
         return(
             <div>   
                 <ViewAssignments/>
-                <h3>View assignments</h3>
-                <UserList list={this.state}/>
+                <div class="view-assignments-container">
+                    <UserList list={this.state}/>
+                </div>
+                <Upload/>
             </div>
         )
     }
