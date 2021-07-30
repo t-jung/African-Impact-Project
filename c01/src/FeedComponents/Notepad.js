58023b99dd0c1f3da7822aae33b28f43919d4e97
UGM <yeonohjung1@gmail.com>
2021-07-30 16:35:36 -0400

    I believe front end of notepad is finished as well, just waiting on backend. fin

--- c01/src/FeedComponents/Notepad.js
+++ c01/src/FeedComponents/Notepad.js
@@ -1,11 +1,57 @@
 import React from 'react'
+import axios from 'axios'
+import { Component } from 'react'
 
-const Notepad = () => {
-  return (
-    <div>
-      Hello
-    </div>
-  )
+class Notepad extends Component {
+  state = {
+       notes: 
+          "Loading notes... "
+    }
+
+
+  componentDidMount() {
+    axios.get(`localhost://5000/api/users/notes/getNotes`)
+    .then(res => {
+        this.setState({videos:res.data});
+        return res.data.map(item =>{ 
+            return {
+                notes: item.notes
+            }    
+        })
+    })
+    .then(res => (
+        this.setState({state: res})
+    ))
+    .catch(err => {
+        console.log(err);
+        this.setState([]);
+    })
+  }
+
+  
+
+  render(){
+    return (
+      <div>
+        <textarea id="w3review" name="w3review" rows="4" cols="50">
+          {this.state.notes}
+        </textarea>
+        <button onClick={saveNote}>
+          save
+        </button>
+      </div>
+    )
+  }
 }
 
 export default Notepad
+
+function saveNote(event) {
+  event.preventDefault();
+  this.setState({
+    notes:this.state.notes
+  }, () => {
+    axios.post('localhost://5000/api/users/notes/addNotes', this.state)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+});}
